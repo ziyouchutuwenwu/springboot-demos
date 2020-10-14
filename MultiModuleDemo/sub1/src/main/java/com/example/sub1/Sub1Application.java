2d3b6e2b1948af9ed48f7fba3faa58986dc4cdf5
unknown <lszcliyez@corp.longshine.com>
2020-10-14 14:46:57 +0800

    multimodules

---
+++ MultiModuleDemo/sub1/src/main/java/com/example/sub1/Sub1Application.java
@@ -0,0 +1,13 @@
+package com.example.sub1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sub1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sub1Application.class, args);
+	}
+
+}
---
+++ MultiModuleDemo/sub1/src/test/java/com/example/sub1/Sub1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sub1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sub1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
